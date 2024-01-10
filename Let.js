01f257b3d3184d0128784dde2a888fa521c5182e
abdulwahabfarooq <abdulwahabfarooq@outlook.com>
2024-01-10 14:42:44 +0500

    Update Let.js

--- Let.js
+++ Let.js
@@ -1,8 +1,9 @@
-{
+
     let blockedVariable = 10;
 console.log(blockedVariable);
-} //it will run
+ //it will run
 
 
 //It will not run because let scope is blocked into curly parenthesis
+let blockedVariable = "Redeclaring";
 console.log(blockedVariable);
