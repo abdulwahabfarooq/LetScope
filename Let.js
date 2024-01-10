4c7b53ef01d352675debdb785b689fbf95d9925f
abdulwahabfarooq <abdulwahabfarooq@outlook.com>
2024-01-10 14:39:31 +0500

    Create Let.js

---
+++ Let.js
@@ -0,0 +1,8 @@
+{
+    let blockedVariable = 10;
+console.log(blockedVariable);
+} //it will run
+
+
+//It will not run because let scope is blocked into curly parenthesis
+console.log(blockedVariable);
